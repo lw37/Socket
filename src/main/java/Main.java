aca5652134d03918e011d7d8e0e5d6786ec95fb2
Wei Luo <luo.luo.ll14@gmail.com>
2021-01-24 16:48:27 +0100

    Solo funciona de lanzar un comando

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,32 +5,27 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final int PORT_HTTP =8989 ;
+    private static final int PORT_HTTP =9876;
 
     public static void main(String[] args) throws IOException {
-
         Socket socket= new Socket();
         socket.connect(new InetSocketAddress("localhost",PORT_HTTP));
         InputStream inputStream=socket.getInputStream();
         OutputStream outputStream=socket.getOutputStream();
         BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream));
-        PrintWriter printer =new PrintWriter(new OutputStreamWriter(outputStream));
+        PrintWriter printer =new PrintWriter(new OutputStreamWriter(outputStream),true);
         Scanner s = new Scanner(System.in);
         String message;
         while(true){
-            message = s.nextLine();
+            message = s.next();
             if(message.equals("exit")){
                 break;
             }
             printer.println(message);
-            printer.flush();
-            System.out.println(reader.readLine());
-        }
-
-
-        String line;
-        while ((line=reader.readLine())!=null){
-            System.out.println((line));
+            String line;
+            while ((line=reader.readLine())!=null){
+                System.out.println((line));
+            }
         }
     }
 }
